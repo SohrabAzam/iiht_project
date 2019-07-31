b91e0827039cc404f477face60a27973f5e2cd47
SohrabAzam <46805192+SohrabAzam@users.noreply.github.com>
2019-07-31 16:40:52 +0530

    Add files via upload

---
+++ Assessment/31-july/hello.js
@@ -0,0 +1,20 @@
+function sayHello() {
+ alert("Hello World, this is from js file");
+}
+
+function add(a, b)
+{
+var c;
+c = a + b;
+console.log('Can u see this?');
+alert('The result is '+c);
+
+}
+
+function myFunction(x) {
+  x.style.background = "red";
+}
+
+function myFunction1(y) {
+  y.style.background = "white";
+}
